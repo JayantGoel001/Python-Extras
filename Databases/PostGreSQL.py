fa4769d0d6812d1c15b0d04ea322cdd647257701
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-04 00:09:56 +0530

    Initial Commit

--- Databases/PostGreSQL.py
+++ Databases/PostGreSQL.py
@@ -9,4 +9,13 @@ def createTable():
     conn.close()
 
 
-createTable()
+def insert(rollNo, name, marks):
+    conn = psycopg2.connect(
+        "dbname='data2' user='postgres' password='postgres' password='postgres' port='5432' host='localhost'")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO data VALUES(%s,%s,%s)", (rollNo, name, marks))
+    conn.commit()
+    conn.close()
+
+
+insert(1, "Pugg Ms", 100.0)
