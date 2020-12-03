0ce15efd27708001dfcf42ebf7aad75e482590ec
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-03 17:00:44 +0530

    Initial Commit

--- Databases/PostGreSQL.py
+++ Databases/PostGreSQL.py
@@ -1,11 +1,9 @@
 import psycopg2
 
-
 def createTable():
     conn = psycopg2.connect(
         "dbname='data2' user='postgres' password='postgres' password='postgres' port='5432' host='localhost'")
     cur = conn.cursor()
-
     cur.execute("Create TABLE data(rollNo INTEGER ,name TEXT,marks REAL)")
     conn.commit()
     conn.close()
--- Databases/SQLite3Basic.py
+++ Databases/SQLite3Basic.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def createTable():
     conn = sqlite3.connect('lite.db')
     cur = conn.cursor()
@@ -8,4 +9,12 @@ def createTable():
     conn.close()
 
 
-createTable()
+def insert(rollNo, name, marks):
+    conn = sqlite3.connect('lite.db')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO data VALUES(?,?,?)", (rollNo, name, marks))
+    conn.commit()
+    conn.close()
+
+
+insert(1, "Pugg Ms", 100.0)
